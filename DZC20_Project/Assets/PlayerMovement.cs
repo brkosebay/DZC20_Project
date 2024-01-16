c2fe21fa8ae57f713865da27dcddc8c723510f98
tomaandreisacuiu <toma.sacuiu.16@gmail.com>
2024-01-16 02:24:09 +0100

    cinemachine virtual camera working

--- DZC20_Project/Assets/PlayerMovement.cs
+++ DZC20_Project/Assets/PlayerMovement.cs
@@ -11,10 +11,15 @@ public class PlayerMovement : MonoBehaviour
 
     public float runSpeed = 40f;
 
+    private BoxCollider2D boundaryCollider;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        // Find the Boundary BoxCollider2D in the scene
+        // If the boundary collider is attached to another object, you will need to find that object specifically
+        // For example, you could use GameObject.FindWithTag if you've tagged your boundary object
+        boundaryCollider = GameObject.FindWithTag("Boundary").GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -28,8 +33,14 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         // Move our character
-        // Assuming your controller.Move method can handle two parameters for horizontal and vertical movement
         controller.Move(horizontalMove * Time.fixedDeltaTime, verticalMove * Time.fixedDeltaTime, false, false);
+
+        // Clamp the player's position within the Box Collider's bounds
+        // This assumes that the boundary collider is axis-aligned and not rotated
+        transform.position = new Vector3(
+            Mathf.Clamp(transform.position.x, boundaryCollider.bounds.min.x, boundaryCollider.bounds.max.x),
+            Mathf.Clamp(transform.position.y, boundaryCollider.bounds.min.y, boundaryCollider.bounds.max.y),
+            transform.position.z
+        );
     }
 }
-
